f29d97a785033dc1628ead19d5c20b21c4e3b596
meel516 <102860983+meel516@users.noreply.github.com>
2024-07-19 04:50:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ db.once("open", () => {
   app.use(express.json())
   app.use(cookieParser());
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true, // Enable sending cookies
   }));
   // app.use(session({
